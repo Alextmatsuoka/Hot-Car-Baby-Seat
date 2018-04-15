93e7c1186c3e80f98a3296d19150e436d7cf70d1
Alextmatsuoka <34973544+Alextmatsuoka@users.noreply.github.com>
2018-04-14 21:09:25 -0700

    Update HotBby.ino

--- HotBby.ino
+++ HotBby.ino
@@ -1,107 +1,125 @@
-#include "Wire.h" //I2C library
-#include "ADXL345.h"
+//Revision 2.0 Ready for demo 4 
+//April 14 2018
+#include <RFM69.h>
+#include <SPI.h>
 #include "RHT03.h"
 #include "ArduinoJson.h"
+// Addresses for this node. CHANGE THESE FOR EACH NODE!
+
+#define NETWORKID   0   // Must be the same for all nodes
+#define MYNODEID    2   // My node ID
+#define TONODEID    1   // Destination node ID
+
+#define FREQUENCY   RF69_915MHZ
+#define ENCRYPT     true // Set to "true" to use encryption
+#define ENCRYPTKEY  "STRING" // Use the same 16-byte key on all nodes
 
 const int RHT03_DATA_PIN = 4; // RHT03 data pin
-float temp;
-float temparray[5];
-int tempPin = A2; // Temperature input pin
-int sampleTime = 1000; // 1 second dafault 
-float occupancy;
-float latestHumidity;
-float latestTempF;
-float severe;
-  
+RFM69 radio;
 RHT03 rht;
-ADXL345 adxl;
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(57600);
-  Wire.begin();
-  adxl.begin();
+void setup()
+{
+  // Open a serial port so we can send keystrokes to the module:
+
+  Serial.begin(9600);
+  Serial.print("Node ");
+  Serial.print(MYNODEID,DEC);
+  Serial.println(" ready");  
   rht.begin(RHT03_DATA_PIN);
-  delay(1000);
-  Serial.setTimeout(500);
+
+  radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
+  radio.setHighPower(); // Always use this for RFM69HCW
+
+  if (ENCRYPT)
+  radio.encrypt(ENCRYPTKEY);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int updateRet = rht.update();
+int turn = 0;
+void loop()
+{
+  // Set up a "buffer" for characters that we'll send:
 
+  static char sendbuffer[62];
+  static int sendlength = 0;
+
+ 
+  if(turn){
+  if (Serial.available() > 0){
+ 
+    char input = Serial.read();
+ 
+    if (input != '\r') // not a carriage return
+      sendbuffer[sendlength++] = input;
+ 
+    // If the input is a carriage return, or the buffer is full:
+ 
+    if ((input == '\r') || (sendlength == 61)){ // CR or buffer full
+   
+      Serial.print("sending to node ");
+      Serial.print(TONODEID, DEC);
+      Serial.print(", message [");
+      for (byte i = 0; i < sendlength; i++)
+        Serial.print(sendbuffer[i]);
+      Serial.println("]");
+        radio.send(TONODEID, sendbuffer, sendlength);
+      sendlength = 0; // reset the packet
+      resetFunc();  //call reset
+    }
+  }
+  turn = !turn;
+  }else{
+  turn = !turn;
   StaticJsonBuffer<200> jsonBuffer;
-  String json = Serial.readStringUntil('}');
-  if(json.length() == 0){
-    json = "{}";
-  }
-  else{
-    json = json + "}";
-  }
+  String json = "{}";
   JsonObject& root = jsonBuffer.parseObject(json);
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
-    Serial.println(json);
-  }
-  
-  for(int i=4; i>0; i--){
-    temparray[i] = temparray[i-1];
-  }
-  
-  temp = analogRead(A2);
-  temp = temp * 0.48828125;
-  temp = temp *9 / 5;
-  temp = temp + 32;
-  temparray[0] = temp;
-  temp = (temparray[0]*3+temparray[1]*4+temparray[2]*3+temparray[3]*2+temparray[4])/13;
-  temparray[0] = temp;
-  
-  // If successful, the update() function will return 1.
-  if (updateRet == 1)
-  {
-    // The humidity(), tempC(), and tempF() functions can be called -- after 
-    // a successful update() -- to get the last humidity and temperature
-    // value 
-    latestHumidity = rht.humidity();
-    latestTempF = rht.tempF();
-  }
-  else
-  {
-    // If the update failed, try delaying for RHT_READ_INTERVAL_MS ms before
-    // trying again.
-    delay(1000);
-  }
+    int updateRet = rht.update();
+    double temp = analogRead(A3);
+    temp = temp * 0.48828125;
+    temp = temp *9 / 5;
+    temp = temp + 32;
+    
+    if (updateRet == 1){
+      double latestHumidity = rht.humidity();
+      double latestTempF = rht.tempF();
+      double dew = pow((latestHumidity/100),(1/8))*(112+.9*(latestTempF))+(.1*(latestTempF)) - 112;
+        root["TEMP"] = (temp);
+        root["HUMIDITY"] = (latestHumidity);
+        root["TEMP_OTHER"] = (latestTempF);
+        root["DEW"] = (dew);
+          int occu = analogRead(A1);
+          int occupancy = 0;
+          if (occu>700){
+            occupancy = 1;
+          }
+          else{
+            occupancy = 0;
+          }
+ 
+          int severe_t = (0.4682*(latestTempF)*(latestTempF) - 56.611*(latestTempF) + 2099.9)*occupancy;
+          if(severe_t>1000){
+            severe_t =1000;
+          }
+          root["SEVERITY_TEMP"] = severe_t;
 
-  /*adxl.read();
-  double xAxis = adxl.getX();
-  double yAxis = adxl.getY();
-  double zAxis = adxl.getZ();
-  long   time  = millis(); 
-*/
-  int occu = analogRead(A1);
-  if (occu>700){
-    occupancy = 1;
-  }
-  else{
-    occupancy = 0;
-  }
+          int severe_d = (0.2526*(dew)*(dew) - 23.64*(dew) + 1012.1) * occupancy;
+          root["SEVERITY_DEWPOINT"] = severe_d;
 
-  severe = (0.4682*(latestTempF)*(latestTempF) - 56.611*(latestTempF) + 2099.9)*occupancy;
-  if(severe>1000){
-    severe =1000;
+          int max_severe;
+          if(severe_t > severe_d){
+            max_severe = severe_t;
+          }
+          else{
+            max_severe = severe_d;
+          }
+          root["MAX_SEVERITY"] = max_severe;
+        root.printTo(Serial);
+        Serial.println();
+    }else{
+      delay(100);
+    }
+
+   
   }
-  root["TEMP"] = latestTempF;
-  root["HUMIDITY"] = latestHumidity; 
-  //root["XAXIS"] = xAxis;
-  //root["YAXIS"] = yAxis;
-  //root["ZAXIS"] = zAxis;
-  root["OCCUPIED"] = occupancy;
-  root["SEVERITY"] = severe;
-  
-  Serial.println();
-//  if(Serial.available()){
-//    root["MSG"] = Serial.readString();
-//  }
-  root.printTo(Serial); 
-  delay(sampleTime);
 }
